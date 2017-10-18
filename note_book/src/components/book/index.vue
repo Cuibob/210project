c5ca8a08dd9aa1807d2f71954050881e51691276
simin <simin183@126.com>
2017-10-18 18:28:52 +0800

    配置前端ui库

---
+++ note_book/src/components/book/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="page-book">
+		<mt-field placeholder="请输入标题"></mt-field>
+		<mt-field type="textarea" placeholder="请输入内容"></mt-field>
+	</div>
+</template>
--- note_book/src/router/index.js
+++ note_book/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/components/index'
+import Book from '@/components/book/index'
 
 Vue.use(Router)
 
@@ -8,8 +9,15 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'home',
+      component: HomePage,
+      children: [
+        {
+          path: '/book',
+          name: 'book',
+          component: Book
+        }
+      ]
     }
   ]
 })
