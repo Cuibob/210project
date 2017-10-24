3af1ab50131b8e22229376b1069bc79e2946c918
simin <simin183@126.com>
2017-10-24 14:51:02 +0800

    添加ajax配置

--- note_book/src/main.js
+++ note_book/src/main.js
@@ -3,10 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 Vue.use(MintUI)
+Vue.prototype.$ajax = axios
 
 Vue.config.productionTip = false
 
