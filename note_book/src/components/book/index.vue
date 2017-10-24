734b8a122fa122cd7d99a141117d2de818cc5187
simin <simin183@126.com>
2017-10-24 16:04:55 +0800

    添加编辑页的ui

--- note_book/src/components/book/index.vue
+++ note_book/src/components/book/index.vue
@@ -1,6 +1,22 @@
 <template>
 	<div class="page-book">
-		<mt-field placeholder="请输入标题"></mt-field>
-		<mt-field type="textarea" placeholder="请输入内容"></mt-field>
+		<mt-field class="b-title" placeholder="请输入标题"></mt-field>
+		<div class="b-ueditor">
+			<div class="u-header">
+				<mt-badge size="small" color="#ccc">H1</mt-badge>
+				<mt-badge size="small" color="#ccc">H2</mt-badge>
+				<mt-badge size="small" color="#ccc">H3</mt-badge>
+				<mt-badge size="small" color="#ccc">H4</mt-badge>
+			</div>
+			<mt-field type="textarea" rows="5" placeholder="请输入内容"></mt-field>
+		</div>
+		<mt-button type="primary" size="large">保存</mt-button>
 	</div>
 </template>
+
+<script>
+import '../../style/book.scss'
+
+export default {
+}
+</script>
